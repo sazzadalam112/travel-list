1bbad5373814645fa9a8d07f214dfce1632c068a
GeekySazzad <71886049+sazzadalam112@users.noreply.github.com>
2023-08-15 23:48:30 +0530

    update controlled element

--- src/App.js
+++ src/App.js
@@ -1,67 +1,90 @@
+import { useState } from "react";
+
 const initialiItems = [
-  { id: 1,description: "Passports",quantity: 2, packed: false},{ id: 1,description: "Socks",quantity: 12, packed: true},
-  { id: 1,description: "Charger",quantity: 2, packed: false},
-];
+    { id: 1,description: "Passports",quantity: 2, packed: false},
+    { id: 2,description: "Socks",quantity: 12, packed: true},
+    { id: 3,description: "Charger",quantity: 1, packed: false},
+  ];
 
-export default function App(){
-  return( <div className="app">
-  <Logo />
-  <Form />
-  <PackingList />
-  <Stats />
- </div> )
+export default function App() {
+    return<div className="app">
+        <Logo />
+        <Form />
+        <Packinglist />
+        <Stats />
+    </div>
 }
-
 function Logo(){
-  return <h1>🌴Far Away👜</h1>
+    return(
+        <h1>🌴Far away👜</h1>
+    )
 }
 function Form(){
-  return( <form className="add-form">
-    <h3>What do you need for your 😍  trip</h3>
-    <select>
-      {/* <option value={1}>1</option>
-      <option value={2}>2</option>
-      <option value={3}>3</option> */}
-      {Array.from({ length: 20},(_,i) => i + 1).map
-      ((num) => (
-        <option value={num}key={num}>{num}</option>
-      ))}
-      </select>
-      <input type="text" placeholder="Item..."/>
-      <button>ADD</button>
+   const [description, setDescription] = useState("");
+   const [quantity,setQuantity] = useState(1);
+
+ 
+    function handleSubmit(e){
+     e.preventDefault();
     
-  </form>
-  )
+     if(!description) return;
+     const newItem = {description , quantity , packed: false, id: Date.now()};
+     console.log(newItem); 
+
+     setDescription("");
+     setQuantity(1);
+    }
+    return(
+        <form className="add-form" onSubmit={handleSubmit}>
+            <h3>What do you need for your 😍 trip?</h3>
+            <select value={quantity} onChange={(e)=> setQuantity (Number(e.target.value))}>
+               {/* <option value={1}>1</option> 
+               <option value={2}>2</option>
+               <option value={3}>3</option> */}
+               {Array.from({length: 20}, (_, i) => i + 1) 
+               .map((num) => (
+                <option value={num} key={num}>{num}</option>
+               ))}
+            </select>
+            <input type="text" placeholder="Item..." 
+             value={description}
+             onChange={(e) => {
+                console.log(e.target.value);
+                setDescription(e.target.value)
+            }}
+             />
+
+            <button>ADD</button>
+        </form>
+    )
 }
-
-
-function PackingList(){
-  return(
-    <div className="list">
-       <ul>
-        {initialiItems.map((item) =>(
-         <Item item = {item} />
-        ))} 
-      </ul>
-</div>
-  )
+function Packinglist() {
+    return(
+        <div className="list">
+        <ul  >
+          {initialiItems.map((item)=> (
+          <Item item={item} key={item.id} />
+          ))}  
+        </ul>
+        </div>
+    )
 }
- function Item({item}) {
-  return(
-    <li>
-      <span style={item.packed ? {textDecoration:"line-through"} : {}}>
-        {item.quantity}{item.description}
-        </span>
-
-      <button>❌</button>
-    </li>
-  )
+function Item({item}){
+    return(
+        <li>
+            <span style={item.packed ? {textDecoration:"line-through"} : {}}>
+             {item.quantity}   
+            {item.description}
+            </span>
+            <button>❌</button>
+            </li>
+    )
 
 }
-  
-function Stats(){
-  return <footer className="stats">
-    <em>👜you have x items of your list, and yu already packed x (x%) </em>
-  </footer>
-}
-
+ function Stats(){
+    return(
+    <footer className="stats">
+        <em>You have X itmes on your list, and you already packed X(X%)</em>
+    </footer>
+    )
+ }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDom from "react-dom/client";
+import  ReactDOM  from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDom.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
-        <App />
+    <App />
     </React.StrictMode>
 )
